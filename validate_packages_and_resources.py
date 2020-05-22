b660fce55115ac3a6bab126c8bee393f3905a6b6
Carles Pina i Estany <carles@pina.cat>
2020-05-22 10:22:17 +0200

    Tries to validate the datapackages and tabular resources

--- validate_packages_and_resources.py
+++ validate_packages_and_resources.py
@@ -1,16 +1,47 @@
+import os
+
 from datapackage import Package
-from datapackage import validate, exceptions
+from datapackage import exceptions
 
 
-package = Package('/home/jen/projects/spi_data_management/git/frictionless-data-packages/10.5281_zenodo.3250980/datapackage.json')
-package.get_resource('ace_chromium_isotope_concentration').read()
-
-descriptor = package.descriptor
-
-try:
-    valid = validate(descriptor)
-except exceptions.ValidationError as exception:
-   for error in exception.errors:
-      print(error)
+def find_data_packages():
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file == 'datapackage.json':
+                yield os.path.join(root, file)
 
 
+def validate_data_packages():
+    for datapackage_path in find_data_packages():
+        package = Package(datapackage_path)
+
+        for resource in package.resources:
+            if resource.valid is False:
+                print(f'Invalid resource {resource} in {datapackage_path}')
+                continue
+
+            if resource.tabular is False:
+                continue
+
+            try:
+                resource.read()
+            except exceptions.ValidationError as exception:
+                for error in exception.errors:
+                    print(error)
+
+
+if __name__ == '__main__':
+    validate_data_packages()
+
+# package = Package('/home/jen/projects/spi_data_management/git/frictionless-data-packages/10.5281_zenodo.3250980/datapackage.json')
+# package.get_resource('ace_chromium_isotope_concentration').read()
+#
+# descriptor = package.descriptor
+#
+# try:
+#     valid = validate(descriptor)
+# except exceptions.ValidationError as exception:
+#    for error in exception.errors:
+#       print(error)
+#
+#
