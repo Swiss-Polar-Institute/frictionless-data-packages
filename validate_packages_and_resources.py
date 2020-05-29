99b551740c1a0582a2f1ffa01467d8069a999723
Carles Pina i Estany <carles@pina.cat>
2020-05-29 16:46:24 +0200

    Delete commented out code

--- validate_packages_and_resources.py
+++ validate_packages_and_resources.py
@@ -90,16 +90,3 @@ if __name__ == '__main__':
         exitcode = 1
 
     sys.exit(exitcode)
-
-# package = Package('/home/jen/projects/spi_data_management/git/frictionless-data-packages/10.5281_zenodo.3250980/datapackage.json')
-# package.get_resource('ace_chromium_isotope_concentration').read()
-#
-# descriptor = package.descriptor
-#
-# try:
-#     valid = validate(descriptor)
-# except exceptions.ValidationError as exception:
-#    for error in exception.errors:
-#       print(error)
-#
-#
