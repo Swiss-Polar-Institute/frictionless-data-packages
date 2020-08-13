f5cad18f3551b3a44a4db5049c2ba115174c4eb7
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-08-13 16:03:42 +0200

    Add new fields to generate_index as a test with example data.

--- tools/generate_index.py
+++ tools/generate_index.py
@@ -13,9 +13,13 @@ def generate_index():
         datapackage_path = os.path.join(directory, 'datapackage.json')
         datapackage = json.load(open(datapackage_path))
         version = datapackage['version']
+        x_spi_datapackage_lastupdate = '2020-08-05 10:00:00'
+        x_spi_tableschema_lastupdate = '2020-08-05 11:00:00'
 
         result.append({'directory': directory,
-                       'version': version
+                       'version': version,
+                       'x_spi_datapackage_lastupdate': x_spi_datapackage_lastupdate,
+                       'x_spi_tableschema_lastupdate': x_spi_tableschema_lastupdate
                        })
 
     json.dump(result, open('index.json', 'w'), sort_keys=True, indent=2)
