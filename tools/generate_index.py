f07155fd4da6dba4550ad8546115869226f3bccc
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-08-13 17:45:05 +0200

    Add todo

--- tools/generate_index.py
+++ tools/generate_index.py
@@ -3,6 +3,19 @@
 import glob
 import json
 import os
+import git
+import time
+
+
+def get_git_commit_time(path):
+
+    repo = git.Repo(search_parent_directories=True)
+    # headcommit = repo.head.commit
+    # commit_time = time.strftime("%a, %d %b %Y %H:%M", time.gmtime(headcommit.committed_date))
+    commit_linux_timestamp = next(repo.iter_commits(paths=path)).committed_date
+    commit_time = time.strftime("%Y-%m-%dT%H:%M:%S%z", time.gmtime(commit_linux_timestamp))
+
+    return commit_time
 
 
 def generate_index():
@@ -12,14 +25,16 @@ def generate_index():
         print(directory)
         datapackage_path = os.path.join(directory, 'datapackage.json')
         datapackage = json.load(open(datapackage_path))
+        # TODO - amend for if there are no, or more than one tableschema files within the path
+        tableschema_path = os.path.join(directory, 'tableschema.json')
         version = datapackage['version']
-        x_spi_datapackage_lastupdate = '2020-08-05 10:00:00'
-        x_spi_tableschema_lastupdate = '2020-08-05 11:00:00'
+        datapackage_lastupdate = get_git_commit_time(datapackage_path)
+        tableschema_lastupdate = get_git_commit_time(tableschema_path)
 
         result.append({'directory': directory,
                        'version': version,
-                       'x_spi_datapackage_lastupdate': x_spi_datapackage_lastupdate,
-                       'x_spi_tableschema_lastupdate': x_spi_tableschema_lastupdate
+                       'x_spi_datapackage_lastupdate': datapackage_lastupdate,
+                       'x_spi_tableschema_lastupdate': tableschema_lastupdate
                        })
 
     json.dump(result, open('index.json', 'w'), sort_keys=True, indent=2)
